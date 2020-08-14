06439745061b30b6bd2e4a873b75cc3c8404820b
Arslanbek Jumabaev <remedy99.aj@gmail.com>
2020-08-14 20:16:05 +0600

    Add adapter file

---
+++ db/adapter.py
@@ -0,0 +1,33 @@
+from models import Book
+
+
+class BooksDBAdapter:
+    def __init__(self):
+        self.books = []
+
+    def prepare(self):
+        pass
+
+    def get_all_books(self) -> Book or None:
+        for book in self.books:
+            if book.id == id:
+                return book
+        return None
+
+    def delete_book(self, id):
+        for book in self.books:
+            if book.id == id:
+                self.books.remove(book)
+                break
+
+    def save_new_book(self, book: Book):
+        self.books.append(book)
+
+    def update_book(self, updated_book: Book):
+        for book in self.books:
+            if book.id == updated_book.id:
+                book.name = updated_book.name
+                book.year = updated_book.year
+                book.author = updated_book.author
+                book.genre = updated_book.genre
+                break
---
+++ models.py
@@ -0,0 +1,18 @@
+class Book:
+    def __init__(self, name, year, author, genre):
+        self.id = None
+        self.name = name
+        self.year = year
+        self.author = author
+        self.genre = genre
+
+    def __repr__(self):
+        return f"{self.id}. {self.name} {self.year}"
+
+
+class BooksApp:
+    def __init__(self, adapter):
+        self.
+
+    def start(self):
+        pass
